1747c9f801899ac648b19c9e711855fc15c88c75
trannhutphat23 <trannhutphattv@gmail.com>
2024-05-02 03:06:22 +0700

    fix accessToken

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -105,7 +105,7 @@ class AccessService {
 
             return {
                 success: true,
-                accessToken,
+                accessToken: accessToken,
             }
             
         } catch (error) {
